001b0bcdddbe80eb9f4b6f980bb797a24756cb33
alevtina-zabegaeva <95501508+alevtina-zabegaeva@users.noreply.github.com>
2023-12-29 22:53:04 +0100

    Add files via upload

--- 18.2.py
+++ 18.2.py
@@ -7,7 +7,6 @@ def main():
         for line in f:
             _, _, n = line.strip().split()
             plan.append((n[-2], int(n[2:-2], 16)))
-    print(plan)
 
     directions = {'3': (-1, 0), '1': (1, 0), '2': (0, -1), '0': (0, 1)}
     trench = [(0, 0)]
@@ -16,8 +15,6 @@ def main():
         dir_i, dir_j = directions[dir]
         current = (current[0] + dir_i*length, current[1] + dir_j*length)
         trench.append(current)
-
-    print(trench)
 
     n = len(trench)
     trench_len = sum(abs(trench[i + 1][0]  - trench[i][0] + trench[i + 1][1] - trench[i][1]) for i in range(n - 1)) // 2 + 1
